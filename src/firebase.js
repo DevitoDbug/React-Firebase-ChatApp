1fc0bc6322e54830bfdb61a692c3b0ede51caa49
DevitoDbug <Davidochiengy@gmail.com>
2023-08-08 23:42:30 +0300

    User authentication using firebase

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+const firebaseConfig = {
+  apiKey: "AIzaSyDmy91s9HIAfNs2vwyKymFd29dnVZm7iqU",
+  authDomain: "artlife3.firebaseapp.com",
+  projectId: "artlife3",
+  storageBucket: "artlife3.appspot.com",
+  messagingSenderId: "688613520295",
+  appId: "1:688613520295:web:955b29cb08dc71ccd57c82"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth()
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,13 +1,34 @@
-import React from "react";
+import { faImage } from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { useState } from "react";
+
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase";
 
 const Register = () => {
-  const handleSubmit = (e) => {
+  const [error, setError] = useState("");
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
+
+    //data from the form
+    const fname = e.target[0].value;
+    const sname = e.target[1].value;
+    const email = e.target[2].value;
+    const password = e.target[3].value;
+    const file = e.target[4].files[0];
+
+    try {
+      const res = await createUserWithEmailAndPassword(auth, email, password);
+      console.log(res);
+    } catch (e) {
+      setError(e.message);
+    }
   };
   return (
     <form
       onSubmit={handleSubmit}
-      className="text-lg w-full h-full bg-C_LightBlue flex  flex-col gap-3 items-center justify-center md:w-[60%] md:h-[40%] md:rounded-xl md:gap-3 md:shadow-lg  lg:w-[3clear0%] lg:h-[60%] "
+      className="text-lg w-full h-full bg-C_LightBlue flex  flex-col gap-3 items-center justify-center md:w-[60%] md:h-[50%] md:rounded-xl md:gap-3 md:shadow-lg  lg:w-[30%] lg:h-[70%] "
     >
       <h2 className="text-C_TextBlack text-xl md:text-2xl lg:text-lg">
         Register
@@ -36,9 +57,23 @@ const Register = () => {
         placeholder="Password"
         name="id"
       />
+
+      <input id="file" type="file" className="hidden" />
+      <label
+        className="flex flex-row items-center cursor-pointer"
+        htmlFor="file"
+      >
+        <FontAwesomeIcon
+          icon={faImage}
+          className="text-C_DarkBlue text-[150%] p-1"
+        />
+        <span className="text-[80%] text-C_TextBlack">Add profile pic</span>
+      </label>
+
       <button className="text-2xl text-C_TextWhite bg-C_DarkBlue shadow-lg py-2 px-3 rounded-xl md:text-3xl md:mt-3 lg:text-xl lg:mt-3">
         Register
       </button>
+      {error && <span className="text-sm text-red-600">{error}</span>}
     </form>
   );
 };
