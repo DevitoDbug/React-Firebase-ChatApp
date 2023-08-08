ac79e823240e3f298cb5fa81436f8f73b230bc92
DevitoDbug <Davidochiengy@gmail.com>
2023-08-08 21:29:06 +0300

    Fixed some bugs

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -7,7 +7,7 @@ const Register = () => {
   return (
     <form
       onSubmit={handleSubmit}
-      className="text-lg w-full h-full bg-C_LightBlue flex  flex-col gap-3 items-center justify-center md:w-[60%] md:h-[40%] md:rounded-xl md:gap-3 md:shadow-lg  lg:w-[30%] lg:h-[60%] "
+      className="text-lg w-full h-full bg-C_LightBlue flex  flex-col gap-3 items-center justify-center md:w-[60%] md:h-[40%] md:rounded-xl md:gap-3 md:shadow-lg  lg:w-[3clear0%] lg:h-[60%] "
     >
       <h2 className="text-C_TextBlack text-xl md:text-2xl lg:text-lg">
         Register
