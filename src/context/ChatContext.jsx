eb7a4cd20b42e091eba8d45cd7f3019ccdcfb85c
DevitoDbug <Davidochiengy@gmail.com>
2023-09-04 19:36:05 +0300

    Implementing useReducer to manage chat  context states

--- src/context/ChatContext.jsx
+++ src/context/ChatContext.jsx
@@ -1,10 +1,38 @@
 const { createContext } = require('react');
-import React from 'react';
+import React, { useContext, useReducer } from 'react';
+import { LoginContext } from './AuthContext';
 
 export const ChatContext = createContext();
 
 const ChatContextProider = ({ children }) => {
-  return <ChatContext.Provider>{children}</ChatContext.Provider>;
+  const { currentUser } = useContext(LoginContext);
+
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case 'CHANGE_CHAT':
+        return {
+          userInfo: action.payload,
+          chatId:
+            currentUser.uid > action.payload.uid
+              ? currentUser.uid + action.payload.uid
+              : currentUser.uid + action.payload.uid,
+        };
+      default:
+        throw new Error();
+    }
+  };
+
+  const INITIAL_STATE = {
+    chatId: '',
+    userInfo: {},
+  };
+
+  const [state, dispatch] = useReducer(reducer, INITIAL_STATE);
+  return (
+    <ChatContext.Provider value={{ data: state, dispatch }}>
+      {children}
+    </ChatContext.Provider>
+  );
 };
 
 export default ChatContextProider;
