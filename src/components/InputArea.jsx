f7bfca2934c582bfd28ee7ea7b6f71437ec36920
DevitoDbug <Davidochiengy@gmail.com>
2023-09-05 13:32:21 +0300

    Fixed the input area - added auto grow

--- src/components/InputArea.jsx
+++ src/components/InputArea.jsx
@@ -9,7 +9,7 @@ import React from 'react';
 const InputArea = () => {
   return (
     <div>
-      <div className="justify-aroun flex h-auto w-full resize-y flex-row items-center gap-2 rounded-lg border-2 border-C_GreyBorder bg-C_WhiteBright p-1">
+      <div className="justify-aroun absolute flex h-auto w-full resize-y flex-row items-center gap-2 rounded-lg border-2 border-C_GreyBorder bg-C_WhiteBright p-1">
         <div className="flex items-center justify-center gap-1">
           <FontAwesomeIcon
             icon={faPaperclip}
@@ -21,13 +21,17 @@ const InputArea = () => {
           />
         </div>
         <textarea
-          className="h-auto w-[70%] resize-y justify-end outline-none"
+          className="h-auto w-[70%] resize-none justify-end outline-none"
           placeholder="Type new message..."
+          onInput={(e) => {
+            e.target.style.height = 'auto';
+            e.target.style.height = `${e.target.scrollHeight}px`;
+          }}
           type="text"
         />
         <FontAwesomeIcon
           icon={faPaperPlane}
-          className=" justify-end text-xl text-C_UserDullBlack"
+          className=" relative right-2 justify-end text-xl text-C_UserDullBlack"
         />
       </div>
     </div>
