6e7b07fd9708914b7b160ce03538e0a3fbfa33a1
DevitoDbug <Davidochiengy@gmail.com>
2023-10-09 23:25:04 +0300

    clean code

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,6 +1,6 @@
-import { createContext, useEffect, useState } from "react";
-import { onAuthStateChanged } from "firebase/auth";
-import { auth } from "../firebase";
+import { createContext, useEffect, useState } from 'react';
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from '../firebase';
 
 export const LoginContext = createContext();
 
