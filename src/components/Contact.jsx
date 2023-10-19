0c56e52559d8864287bea31742db46b229133944
devitoDbug <davidOchiengy@gmail.com>
2023-10-20 00:41:23 +0300

    refactor: Remove unnecessary code

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -107,7 +107,6 @@ const Contact = ({
     // eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions
     <div
       onClick={() => {
-        console.log(onClick);
         handleSelect();
         onClick();
       }}
