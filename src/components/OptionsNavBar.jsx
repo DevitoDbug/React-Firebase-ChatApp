dc20ea9675da5e0d95b89cf270582a090069e1f2
devitoDbug <davidOchiengy@gmail.com>
2023-10-20 09:47:44 +0300

    chore: Move message icon to a more convenient position

--- src/components/OptionsNavBar.jsx
+++ src/components/OptionsNavBar.jsx
@@ -24,6 +24,22 @@ const OptionsNavBar = () => {
 
   return (
     <div className="flex w-full flex-row items-center justify-center gap-1 border-t-2 border-C_BorderLightBlue p-2">
+      <div className="ml-2 flex flex-col items-center justify-center">
+        <button>
+          <FontAwesomeIcon
+            icon={faStar}
+            className="text-xl text-C_UserDullBlack"
+          />
+        </button>
+        <div
+          className={`h-1 w-[75%] rounded-xl  ${
+            currentPage == 'starPage'
+              ? 'bg-C_DarkBlue'
+              : 'bg-transparent'
+          }`}
+        ></div>
+      </div>
+
       <div className=" flex flex-col items-center justify-center">
         <button>
           <FontAwesomeIcon
@@ -41,38 +57,6 @@ const OptionsNavBar = () => {
       </div>
 
       <div className="ml-2 flex flex-col items-center justify-center">
-        <button onClick={scrollToMessageSection}>
-          <FontAwesomeIcon
-            icon={faMessage}
-            className=" text-xl text-C_UserDullBlack"
-          />
-        </button>
-        <div
-          className={`h-1 w-[75%] rounded-xl  ${
-            currentPage == 'contactPage'
-              ? 'bg-C_DarkBlue'
-              : 'bg-transparent'
-          }`}
-        ></div>
-      </div>
-
-      <div className="ml-2 flex flex-col items-center justify-center">
-        <button>
-          <FontAwesomeIcon
-            icon={faStar}
-            className="text-xl text-C_UserDullBlack"
-          />
-        </button>
-        <div
-          className={`h-1 w-[75%] rounded-xl  ${
-            currentPage == 'starPage'
-              ? 'bg-C_DarkBlue'
-              : 'bg-transparent'
-          }`}
-        ></div>
-      </div>
-
-      <div className="ml-2 flex flex-col items-center justify-center">
         <button onClick={handleOpenSearch}>
           <FontAwesomeIcon
             icon={faSearch}
@@ -82,6 +66,22 @@ const OptionsNavBar = () => {
         <div
           className={`h-1 w-[75%] rounded-xl  ${
             currentPage == 'searchPage'
+              ? 'bg-C_DarkBlue'
+              : 'bg-transparent'
+          }`}
+        ></div>
+      </div>
+
+      <div className="ml-2 flex flex-col items-center justify-center">
+        <button onClick={scrollToMessageSection}>
+          <FontAwesomeIcon
+            icon={faMessage}
+            className=" text-xl text-C_UserDullBlack"
+          />
+        </button>
+        <div
+          className={`h-1 w-[75%] rounded-xl  ${
+            currentPage == 'contactPage'
               ? 'bg-C_DarkBlue'
               : 'bg-transparent'
           }`}
