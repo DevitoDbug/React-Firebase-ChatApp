0cbec87356560264e10e1c48f44baf1fce7bad8a
devitoDbug <davidOchiengy@gmail.com>
2023-10-22 22:40:02 +0300

    fix: Fix some bugs

--- src/components/UserProfileSection.jsx
+++ src/components/UserProfileSection.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 
 const UserProfileSection = () => {
-  return <div>
+  return (
     <div>
-      <h2>Profile</h2>
-      <p>Username: <span>username</span></p>
-      <p>Email: <span>email</span></p>
-  
+      <div>
+        <h2>Profile</h2>
+        <p>
+          Username: <span>username</span>
+        </p>
+        <p>
+          Email: <span>email</span>
+        </p>
+        <div>Change profile picture</div>
+      </div>
     </div>
-  </div>;
+  );
 };
 
 export default UserProfileSection;
