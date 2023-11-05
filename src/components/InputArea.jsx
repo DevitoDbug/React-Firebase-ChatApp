25921cbc447c5594358c222e1b6862a5118ffd61
devitoDbug <davidOchiengy@gmail.com>
2023-11-05 22:16:36 +0300

    Fix bug: Removed unintended position: absolute from the code.

--- src/components/InputArea.jsx
+++ src/components/InputArea.jsx
@@ -80,8 +80,8 @@ const InputArea = () => {
   };
 
   return (
-    <div className="">
-      <div className=" absolute flex h-auto w-[95%] resize-y flex-row items-center justify-around gap-2 rounded-lg border-2 border-C_GreyBorder bg-C_WhiteBright  p-1">
+    <div className="md:w-[67vw]">
+      <div className="flex h-auto w-[95%] resize-y flex-row items-center justify-around gap-2 rounded-lg border-2 border-C_GreyBorder bg-C_WhiteBright  p-1">
         <div className="flex items-center justify-center gap-1 ">
           <input
             onChange={(e) => setImage(e.target.files[0])}
